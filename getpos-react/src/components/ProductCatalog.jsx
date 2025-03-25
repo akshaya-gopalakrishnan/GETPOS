4956bd7f4a437945eb379bffb4a133fb422acaf4
Rahla <rahilana88@gmail.com>
2025-03-25 13:44:22 +0400

    product-catlogue updated

--- getpos-react/src/components/ProductCatalog.jsx
+++ getpos-react/src/components/ProductCatalog.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
-import Pagination from "./pagination"; // Import the Pagination component
+// import Pagination from "./pagination"; // Import the Pagination component
 
 const ProductCatalog = ({ categoryName, products, onAddToCart }) => {
   const [currentPage, setCurrentPage] = useState(1);  // Page state
@@ -12,68 +12,61 @@ const ProductCatalog = ({ categoryName, products, onAddToCart }) => {
   }, [categoryName]);
 
   // Adjust number of products per page based on screen size
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width >= 1720) {
-        setProductsPerPage(10);
-      } else if (width >= 1430) {
-        setProductsPerPage(8);
-      } else if (width >= 1230) {
-          setProductsPerPage(8);
-        }
-      else if (width >= 1150) {
-        setProductsPerPage(6);
-      } else {
-        setProductsPerPage(4);
-      }
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const width = window.innerWidth;
+  //     if (width >= 1720) {
+  //       setProductsPerPage(10);
+  //     } else if (width >= 1430) {
+  //       setProductsPerPage(8);
+  //     } else if (width >= 1230) {
+  //         setProductsPerPage(8);
+  //       }
+  //     else if (width >= 1150) {
+  //       setProductsPerPage(6);
+  //     } else {
+  //       setProductsPerPage(4);
+  //     }
+  //   };
 
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Set initial value based on current window size
+  //   window.addEventListener("resize", handleResize);
+  //   handleResize(); // Set initial value based on current window size
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize",handleResize);
+  //   };
+  // }, []);
 
   // Calculate the indexes for slicing the products array
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);  // Current page products
+  // const indexOfLastProduct = currentPage * productsPerPage;
+  // const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  // const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);  // Current page products
 
   // Determine total number of pages
-  const totalPages = Math.ceil(products.length / productsPerPage);
+  // const totalPages = Math.ceil(products.length / productsPerPage);
 
   // Handle page changes
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage); // Update to the selected page
-  };
+  // const handlePageChange = (newPage) => {
+  //   setCurrentPage(newPage); // Update to the selected page
+  // };
 
   return (
-    <div className="product-catalog">
-      <h2>{categoryName}</h2>
-      <div className="product-list">
-        {currentProducts.length > 0 ? (
-          currentProducts.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              onAddToCart={onAddToCart}
-            />
-          ))
-        ) : (
-          <p>No products available</p>
-        )}
-      </div>
-      {totalPages > 1 && (
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}  // Direct page change function
-        />
+    <div className="product-catalog ">
+    <h2>{categoryName}</h2>
+    <div className="product-list">
+      {products.length > 0 ? (
+        products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            onAddToCart={onAddToCart}
+          />
+        ))
+      ) : (
+        <p>No products available</p>
       )}
     </div>
+  </div>
   );
 };
 
